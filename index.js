e4854f176632d1c9f6627010affb459e7c9d63a5
Bob <updatesvc@gmail.com>
2022-09-28 18:49:45 +0300

    feat: trading logic

--- index.js
+++ index.js
@@ -49,3 +49,93 @@ async function checker() {
     console.log(`[Checker]: Bot stopped`)
 
 }
+async function getDirection(liveGame) {
+    //TODO IMPORTANT: change the direcion or use you own logic to instruct the bot to make call, you can use the liveGame variable to generate you own trading/betting logic
+    console.log(`[Logic]: Please review code and add your trading logic on line:49, remember to remove the exit call`)
+    console.log(`[Logic]: Program will now exit`)
+    process.exit()
+    let direction = "up"
+    return direction
+}
+async function tradeEnter(liveGame) {
+    console.log(`[Trade Enter] Start:${new Date().toISOString()}`, liveGame)
+
+    //get block game
+    let game = await getBlockchainRound(liveGame.epoch)
+
+    let direction = getDirection()
+    let bet = { epoch: game.epoch, game, time: new Date() }
+
+    if (direction == "up") {
+        // direction up
+        console.log(`[Trade Enter]: Epoch: ${game.epoch} bet bull`)
+        await betBull(bet)
+    } else {
+        // direction down           
+        console.log(`[Trade Enter]: Epoch: ${game.epoch} bet bear`)
+        await betBear(bet)
+    }
+
+}
+
+async function betBear(bet) {
+    let stake = process.env.STAKE
+    bet.stake = stake
+    let secondsLeft = (bet.game.lockTimestamp - Date.now()) / 1000
+    console.log(`[BetBear]:${bet.epoch}  BearTime ${new Date().toISOString()}  secondsLeft=${secondsLeft}`, bet)
+    let txInfo = await contract.methods.betBear(Number(bet.epoch)).send({
+        gas: 200_000,
+        gasPrice: web3.utils.toWei(process.env.GAS_PRICE.toString(), "Gwei"),
+        from: account.address,
+        value: web3.utils.toWei(stake.toString(), "ether")
+    })
+    console.log(`[BetBear]:End ${bet.epoch} secondsLeft=${secondsLeft}  txInfo`, txInfo)
+
+}
+
+async function betBull(bet) {
+    let stake = process.env.STAKE
+    bet.stake = stake
+    let secondsLeft = (bet.game.lockTimestamp - Date.now()) / 1000
+    console.log(`[BetBull]:${bet.epoch} BullTime ${new Date().toISOString()} secondsLeft=${secondsLeft}`, bet)
+    let txInfo = await contract.methods.betBull(Number(bet.epoch)).send({
+        gas: 200_000,
+        gasPrice: web3.utils.toWei(process.env.GAS_PRICE.toString(), "Gwei"),
+        from: account.address,
+        value: web3.utils.toWei(stake.toString(), "ether")
+    })
+    console.log(`[BetBull]:End ${bet.epoch} secondsLeft = ${secondsLeft}  txInfo`, txInfo)
+}
+async function getLiveGame(retries = 2) {
+    try {
+        let epoch = await contract.methods.currentEpoch().call()
+        //get block round
+        let round = await getBlockchainRound(epoch)
+        return round
+    } catch (ex) {
+        retries++
+        if (retries > 100) {
+            throw ex
+        } else {
+            await sleep(1000 * retries)
+            return await getLiveGame(retries)
+        }
+    }
+
+}
+async function getBlockchainRound(epoch) {
+    const blockRound = await contract.methods.rounds(epoch).call()
+    blockRound.bearAmount = Number(web3.utils.fromWei(blockRound[10], "ether"))
+    blockRound.bullAmount = Number(web3.utils.fromWei(blockRound[9], "ether"))
+    blockRound.lockPrice = Number(blockRound.lockPrice) / 1e8
+    blockRound.closePrice = Number(blockRound.closePrice) / 1e8
+    blockRound.total = blockRound.bearAmount + blockRound.bullAmount
+    blockRound.bullOdds = blockRound.bearAmount / blockRound.bullAmount
+    blockRound.bearOdds = blockRound.bullAmount / blockRound.bearAmount
+    blockRound.startTimestamp = Number(blockRound.startTimestamp) * 1000
+    blockRound.lockTimestamp = Number(blockRound.lockTimestamp) * 1000
+    blockRound.closeTimestamp = Number(blockRound.closeTimestamp) * 1000
+    blockRound.lockTime = new Date(blockRound.lockTimestamp)
+    blockRound.startTime = new Date(blockRound.startTimestamp)
+    return blockRound
+}
